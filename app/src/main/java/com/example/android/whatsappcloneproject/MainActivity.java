7d1cb41704de6a4328d15d472c86dab6caeef96e
woodiechek <ardianiqbal40@gmail.com>
2021-07-28 22:18:08 +0700

    Update MainActivity (27 July 2021)
    Menambahkan item Menu Toolbar
    {UI/LOGIC 100%}

--- app/src/main/java/com/example/android/whatsappcloneproject/MainActivity.java
+++ app/src/main/java/com/example/android/whatsappcloneproject/MainActivity.java
@@ -10,6 +10,9 @@ import androidx.fragment.app.FragmentPagerAdapter;
 import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.example.android.whatsappcloneproject.databinding.ActivityMainBinding;
 import com.example.android.whatsappcloneproject.menu.CallsFragment;
@@ -30,6 +33,7 @@ public class MainActivity extends AppCompatActivity {
 
         setUpWithViewPager(binding.viewPager);
         binding.tabLayout.setupWithViewPager(binding.viewPager);
+        setSupportActionBar(binding.toolbar);
     }
 
     private void setUpWithViewPager(ViewPager viewPager){
@@ -73,7 +77,29 @@ public class MainActivity extends AppCompatActivity {
         public CharSequence getPageTitle(int position) {
             return mFragmentTitleList.get(position);
         }
-
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        // Handler action bar item, logicnya ditaro disini
+        // Action bar akan selamanya menangani on click disini
+        // tanpa harus membuat onClickListener, selama fungsi ini
+        // ditetapkan di android manifest sebagai parent activity
+
+        int id = item.getItemId();
+
+        switch (id){
+            case R.id.menu_search :
+                Toast.makeText(MainActivity.this, "Action Search", Toast.LENGTH_SHORT).show();break;
+            case R.id.menu_more:
+                Toast.makeText(MainActivity.this, "Menu Search", Toast.LENGTH_SHORT).show();break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
