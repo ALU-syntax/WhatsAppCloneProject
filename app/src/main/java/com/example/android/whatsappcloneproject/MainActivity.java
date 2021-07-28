87a813a6d34fab6291add8da7cf4b3171296e269
woodiechek <ardianiqbal40@gmail.com>
2021-07-28 19:27:19 +0700

    Update Activity (28 July 2021)
    Update Main Activity
    Menambahkan 3 Fragment
    Update Manifest
    Update Gradle

--- app/src/main/java/com/example/android/whatsappcloneproject/MainActivity.java
+++ app/src/main/java/com/example/android/whatsappcloneproject/MainActivity.java
@@ -1,14 +1,79 @@
 package com.example.android.whatsappcloneproject;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
+import com.example.android.whatsappcloneproject.databinding.ActivityMainBinding;
+import com.example.android.whatsappcloneproject.menu.CallsFragment;
+import com.example.android.whatsappcloneproject.menu.ChatsFragment;
+import com.example.android.whatsappcloneproject.menu.StatusFragment;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
+        setUpWithViewPager(binding.viewPager);
+        binding.tabLayout.setupWithViewPager(binding.viewPager);
     }
+
+    private void setUpWithViewPager(ViewPager viewPager){
+        MainActivity.SectionPagerAdapter adapter = new SectionPagerAdapter(getSupportFragmentManager());
+
+        adapter.addFragment(new ChatsFragment(), "Chats");
+        adapter.addFragment(new StatusFragment(), "Status");
+        adapter.addFragment(new CallsFragment(), "Calls ");
+
+        //kita membutuhkan 3 fragment
+        viewPager.setAdapter(adapter);
+
+    }
+
+    //Pager Adapter kita taruh di inner class dalam main activity
+    private static class SectionPagerAdapter extends FragmentPagerAdapter{
+
+        private final List<Fragment> mFragmentList = new ArrayList<>();
+        private final List<String> mFragmentTitleList = new ArrayList<>();
+
+        public SectionPagerAdapter(FragmentManager manager) {
+            super(manager);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return mFragmentList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return mFragmentList.size();
+        }
+
+        public void addFragment(Fragment fragment, String title){
+            mFragmentList.add(fragment);
+            mFragmentTitleList.add(title);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mFragmentTitleList.get(position);
+        }
+
+    }
+
 }
