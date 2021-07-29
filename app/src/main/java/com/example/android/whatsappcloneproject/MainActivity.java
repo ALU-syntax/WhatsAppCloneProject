315936706c022de5fff67aed14e3f49a06f2f0df
woodiechek <ardianiqbal40@gmail.com>
2021-07-29 23:30:47 +0700

    Update Fragment Status (29 July 2021)
    fragment Status (UI 100%)

--- app/src/main/java/com/example/android/whatsappcloneproject/MainActivity.java
+++ app/src/main/java/com/example/android/whatsappcloneproject/MainActivity.java
@@ -1,7 +1,6 @@
 package com.example.android.whatsappcloneproject;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
