ad5e3c7258e16a9409774ec39383c9d66b3685d7
woodiechek <ardianiqbal40@gmail.com>
2021-08-01 03:07:05 +0700

    Update ProfileActivity
    add Bottom_sheet_pick xml layout
    logic 50%

--- app/src/main/java/com/example/android/whatsappcloneproject/view/profile/ProfileActivity.java
+++ app/src/main/java/com/example/android/whatsappcloneproject/view/profile/ProfileActivity.java
@@ -1,17 +1,35 @@
 package com.example.android.whatsappcloneproject.view.profile;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
+import android.content.DialogInterface;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
 
 import com.example.android.whatsappcloneproject.R;
 import com.example.android.whatsappcloneproject.databinding.ActivityProfileBinding;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.bottomsheet.BottomSheetDialog;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.mikhaellopez.circularimageview.CircularImageView;
+
+import java.util.Objects;
 
 public class ProfileActivity extends AppCompatActivity {
 
-    ActivityProfileBinding binding;
+    private ActivityProfileBinding binding;
+    private FirebaseUser firebaseUser;
+    private FirebaseFirestore firestore;
+
+    private BottomSheetDialog bottomSheetDialog;
 
     CircularImageView circularImageView;
 
@@ -19,5 +37,65 @@ public class ProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = DataBindingUtil.setContentView(this, R.layout.activity_profile);
+
+        setSupportActionBar(binding.toolbar);
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+
+        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+        firestore = FirebaseFirestore.getInstance();
+
+        if (firebaseUser != null){
+            getInfo();
+        }
+        
+        initActionClick();
+    }
+
+    private void initActionClick() {
+        binding.fabCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showBottomSheetPickPhoto();
+            }
+        });
+    }
+
+    private void showBottomSheetPickPhoto() {
+        View view = getLayoutInflater().inflate(R.layout.bottom_sheet_pick, null);
+
+        bottomSheetDialog = new BottomSheetDialog(this);
+        bottomSheetDialog.setContentView(view);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP_MR1){
+            Objects.requireNonNull(bottomSheetDialog.getWindow()).addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        }
+
+        bottomSheetDialog.setOnDismissListener(new DialogInterface.OnDismissListener() {
+            @Override
+            public void onDismiss(DialogInterface dialog) {
+                bottomSheetDialog = null;
+            }
+        });
+        bottomSheetDialog.show();
+    }
+
+    private void getInfo() {
+        firestore.collection("Users").document(firebaseUser.getUid()).get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+            @Override
+            public void onSuccess(DocumentSnapshot documentSnapshot) {
+                String userName = documentSnapshot.getString("userName");
+                String userPhone = documentSnapshot.getString("userPhone");
+
+                binding.tvUsername.setText(userName);
+                binding.tvPhone.setText(userPhone);
+
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+
+            }
+        });
+
     }
 }
