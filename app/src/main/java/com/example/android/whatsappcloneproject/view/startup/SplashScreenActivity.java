531c9a4f22e12d95d76a7be8b53df6c5b89f15e2
woodiechek <ardianiqbal40@gmail.com>
2021-07-31 20:29:46 +0700

    Update Layout Fragment
    Updatee SplahsScreen Logic
    Update FragmentChats UI
    Update FragmentCalls UI

--- app/src/main/java/com/example/android/whatsappcloneproject/view/startup/SplashScreenActivity.java
+++ app/src/main/java/com/example/android/whatsappcloneproject/view/startup/SplashScreenActivity.java
@@ -7,20 +7,37 @@ import android.os.Bundle;
 import android.os.Handler;
 
 import com.example.android.whatsappcloneproject.R;
+import com.example.android.whatsappcloneproject.view.MainActivity;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class SplashScreenActivity extends AppCompatActivity {
+
+    FirebaseUser firebaseUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
 
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                startActivity(new Intent(SplashScreenActivity.this, WelcomeScreenActivity.class));
-                finish();
-            }
-        }, 3500);
+        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+
+        if (firebaseUser != null){
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    startActivity(new Intent(SplashScreenActivity.this, MainActivity.class));
+                    finish();
+                }
+            }, 3500);
+        } else{
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    startActivity(new Intent(SplashScreenActivity.this, WelcomeScreenActivity.class));
+                    finish();
+                }
+            }, 3500);
+        }
     }
 }
