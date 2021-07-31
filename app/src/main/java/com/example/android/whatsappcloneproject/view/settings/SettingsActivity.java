48cc4e566e82da603cb939f8c505ca2d27952da9
woodiechek <ardianiqbal40@gmail.com>
2021-07-31 23:40:49 +0700

    Update SettingActivity
    Logic 100%

--- app/src/main/java/com/example/android/whatsappcloneproject/view/settings/SettingsActivity.java
+++ app/src/main/java/com/example/android/whatsappcloneproject/view/settings/SettingsActivity.java
@@ -1,16 +1,57 @@
 package com.example.android.whatsappcloneproject.view.settings;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import com.example.android.whatsappcloneproject.R;
+import com.example.android.whatsappcloneproject.databinding.ActivityMainBinding;
+import com.example.android.whatsappcloneproject.databinding.ActivitySettingsBinding;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.Objects;
 
 public class SettingsActivity extends AppCompatActivity {
+
+    private ActivitySettingsBinding binding;
+    private FirebaseUser firebaseUser;
+    private FirebaseFirestore firestore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_settings);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_settings);
+
+        firestore = FirebaseFirestore.getInstance();
+        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+
+        if (firebaseUser != null){
+            getInfo();
+        }
+
+
+    }
+
+    private void getInfo(){
+        firestore.collection("Users").document(firebaseUser.getUid()).get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+            @Override
+            public void onSuccess(DocumentSnapshot documentSnapshot) {
+                String userName = Objects.requireNonNull(documentSnapshot.get("userName")).toString();
+                binding.tvUsername.setText(userName);
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.d("Get Data", "onFailure: " + e.getMessage());
+            }
+        });
     }
 }
