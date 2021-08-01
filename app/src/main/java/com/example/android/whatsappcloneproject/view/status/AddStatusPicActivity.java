ccb69f6fe07ab7ca04b4afa27388717ad7f04358
woodiechek <ardianiqbal40@gmail.com>
2021-08-01 17:05:59 +0700

    FINISH

---
+++ app/src/main/java/com/example/android/whatsappcloneproject/view/status/AddStatusPicActivity.java
@@ -0,0 +1,16 @@
+package com.example.android.whatsappcloneproject.view.status;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.android.whatsappcloneproject.R;
+
+public class AddStatusPicActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_status_pic);
+    }
+}
